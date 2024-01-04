b4f82cd99f4c07f6ec0cc33aad4c78a5f06e10ce
Yuvam <67573209+yuvam2017@users.noreply.github.com>
2024-01-04 13:36:07 +0530

    Add files via upload

---
+++ vlc-yt.bash
@@ -0,0 +1,6 @@
+sudo apt install wget curl
+wget https://raw.githubusercontent.com/yuvam2017/11/master/youtube.lua
+sudo mv youtube.lua /usr/lib/x86_64-linux-gnu/vlc/lua/playlist/
+sudo mv /usr/lib/x86_64-linux-gnu/vlc/lua/playlist/youtube.luac ./
+sudo mv youtube.luac keep_this_file.luac
+echo "All set !!"
